f89db6d0804664466b5e52b984fae9040604c5f9
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-07-20 20:00:36 +0530

    Update cleaner.py

--- Exon/modules/cleaner.py
+++ Exon/modules/cleaner.py
@@ -273,7 +273,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "𝐂ʟᴇᴀɴ"
+__mod_name__ = "Clean"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
