c1c95ff6859b808a5fc015b6872b57e8843395a1
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-07-24 19:18:48 +0530

    Update afk.py

--- Exon/modules/afk.py
+++ Exon/modules/afk.py
@@ -29,7 +29,7 @@ SOFTWARE.
 
 
 import time
-
+import random
 from Abg.helpers.human_read import get_readable_time
 from pyrogram.types import Message
 
@@ -188,9 +188,47 @@ async def active_afk(_, message: Message):
         }
 
     await add_afk(user_id, details)
-    await message.reply_sticker(
-        "CAACAgUAAx0CUgguZAABAdegY2N5paaiPapUxRm0RYy9Xf6dPEYAAisIAAJ2PRlXxkn7UgOIdewqBA"
-    )
+
+    sticker_ids = [
+        "CAACAgEAAxkBAAEJzIFkvnqdG0WplzGQ5pzSO_5o3Sfv5gACeQEAArr88hkbuqYUJn-say8E",
+        "CAACAgEAAxkBAAEJzINkvnrKHt550W3T1xzBq-6DVsjZxgAC3AADuvzyGZMu1owN01-yLwQ",
+        "CAACAgEAAxkBAAEJzMFkvnwtD8_mfgM01BzVTlug0dkE-wACFQEAArr88hm3XMuZH18jhi8E",
+        "CAACAgEAAxkBAAEJzMNkvnw1eo-F4g9yJYquKLmZSV537QACGwEAArr88hm8Bk97XxFmJC8E",
+        "CAACAgEAAxkBAAEJzIFkvnqdG0WplzGQ5pzSO_5o3Sfv5gACeQEAArr88hkbuqYUJn-say8E",
+        "CAACAgEAAxkBAAEJzINkvnrKHt550W3T1xzBq-6DVsjZxgAC3AADuvzyGZMu1owN01-yLwQ",
+        "CAACAgEAAxkBAAEJzIVkvnrlZFHUDst4vF661MfcfSza8gAC-wADuvzyGeFkCRSA-rCGLwQ",
+        "CAACAgEAAxkBAAEJzIdkvnrwNSXJRwyOW-ZOtU1iD0n3TgAC4QADuvzyGagN2YYz-z1xLwQ",
+        "CAACAgEAAxkBAAEJzIlkvnr16zvhOgSSTrorQiRt73FWhgACKgEAArr88hlsLpLpXlzm2C8E",
+        "CAACAgEAAxkBAAEJzItkvnr5gkfHpr2nGXJtUQVKx2VHzwACKQEAArr88hmT3XyhkRmDei8E",
+        "CAACAgEAAxkBAAEJzI1kvnsDHlGtCkrHh6QyIGFt7qDzpgACGAEAArr88hlkt1cr11v0ti8E",
+        "CAACAgEAAxkBAAEJzJFkvnsTCmYxnn7VqDoUvyNde_RPUQACLQEAArr88hle-kwmp_O0hy8E",
+        "CAACAgEAAxkBAAEJzJNkvnspc5eUbUo8dlQxgMJjX1vExQAC9wADuvzyGZpOxvQ3n6KfLwQ",
+        "CAACAgEAAxkBAAEJzJVkvns2exC2Lc3O0Vp_pmyPTyPTuAACFwEAArr88hkRViTRyl-vBS8E",
+        "CAACAgEAAxkBAAEJzJdkvns6XofBV0h5cHKXv6l3uKHhAwAC-gADuvzyGVzrNc6kcC7bLwQ",
+        "CAACAgEAAxkBAAEJzJlkvntK5d6YqYXSgfybZrVoedloJAAC9QADuvzyGcwbuNY211RpLwQ",
+        "CAACAgEAAxkBAAEJzJ1kvntVaklCLuXHtrV-aiUKPb2_FwACJQEAArr88hk85jb83qRcny8E",
+        "CAACAgEAAxkBAAEJzJ9kvntwdnO3GNvDpaVB9NA9az_OFQAC7gADuvzyGeXuyBUPfIBFLwQ",
+        "CAACAgEAAxkBAAEJzKFkvnt8ClZeP29o_A_r4SwopEYQcAAC6gADuvzyGTC36r-q2nqYLwQ",
+        "CAACAgEAAxkBAAEJzKNkvnuMIh27WuIMT2A0YqbMU5GdqwAC7QADuvzyGUxh1Clr8jwULwQ",
+        "CAACAgEAAxkBAAEJzKVkvnuYDT0LstwiE3mwZptwVZAVHwAC8QADuvzyGaeHW-H86XCULwQ",   
+        "CAACAgEAAxkBAAEJzKdkvnudBSwfmcr9yyHuESHf2kX0dQACJAEAArr88hnsckruLKDxWC8E",
+        "CAACAgEAAxkBAAEJzKlkvnut_upII3cdk-Tcmq-2EEajZAACCAEAArr88hl23EpXsYluSy8E",
+        "CAACAgEAAxkBAAEJzKtkvnu2CXEB4xly9725LF4g2IAo-wACCgEAArr88hlKnB_mzREoQC8E",
+        "CAACAgEAAxkBAAEJzK1kvnu-Qb9iQDKN0-Ju17RsAqYs2wACKAEAArr88hmIrG2eEuPKGy8E",
+        "CAACAgEAAxkBAAEJzK9kvnvFAax52rwrTTgE2cC6-DdhgwACfAEAArr88hlGCxAcoCx3Xi8E",
+        "CAACAgEAAxkBAAEJzLFkvnvS1aKAcVTayXTIm24PJfG1kwAC_gADuvzyGXdrX7pwtRlyLwQ",
+        "CAACAgEAAxkBAAEJzLNkvnvgPi46E8v1x1fv8MxC4s_4ogAC9gADuvzyGao46Ts99csiLwQ",
+        "CAACAgEAAxkBAAEJzLVkvnvo60azdj7YpBiFOJP1KOn_dQACegEAArr88hkfC8tRD0R7pi8E",
+        "CAACAgEAAxkBAAEJzLdkvnvs0R9CbM3S3tRuBe2-fSC-owACAwEAArr88hkSA3kXL2h4ky8E",
+        "CAACAgEAAxkBAAEJzLlkvnvzMtEvPXeoS0z3q6Q1361KWQACAQEAArr88hmoeqKPr9utMy8E",
+        "CAACAgEAAxkBAAEJzLtkvnwBiTFevMzbupp9AAFa2qOw73sAAgIBAAK6_PIZ_OQ1ePi_xu8vBA",
+        "CAACAgEAAxkBAAEJzL1kvnwQ0Uunt4AxD7U5Vdl2GszHZgACBQEAArr88hlE1ktsrcJUtC8E",
+        "CAACAgEAAxkBAAEJzL9kvnwa9X9mvBy3rCJIn7mR8QIajAACBwEAArr88hl5S9mKzGwboi8E",
+        "CAACAgEAAxkBAAEJzMFkvnwtD8_mfgM01BzVTlug0dkE-wACFQEAArr88hm3XMuZH18jhi8E",
+        "CAACAgEAAxkBAAEJzMNkvnw1eo-F4g9yJYquKLmZSV537QACGwEAArr88hm8Bk97XxFmJC8E",
+    ]
+    
+    await message.reply_sticker(random.choice(sticker_ids))
     await message.reply_text(f"{message.from_user.first_name} ɪs ɴᴏᴡ ᴀғᴋ!")
 
 
