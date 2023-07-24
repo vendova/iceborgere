b8aa4578dc4e2a7a2aa6450377fe292ebe9e4ceb
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-07-24 19:30:39 +0530

    Update misc.py

--- Exon/modules/helper_funcs/misc.py
+++ Exon/modules/helper_funcs/misc.py
@@ -130,15 +130,15 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * COLUMN_SIZE : COLUMN_SIZE * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "☜",
+                    "⬅️",
                     callback_data="{}_prev({})".format(prefix, modulo_page),
                 ),
                 EqInlineKeyboardButton(
-                    "ɢᴏ ʜᴏᴍᴇ",
+                    "🏠",
                     callback_data="start_back",
                 ),
                 EqInlineKeyboardButton(
-                    "☞",
+                    "➡️",
                     callback_data="{}_next({})".format(prefix, modulo_page),
                 ),
             )
