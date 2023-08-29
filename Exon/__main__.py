f86690a41e3685b2ce44a73f22b697d30baa61bc
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-08-29 15:46:03 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -323,7 +323,7 @@ def help_button(update: Update, context: CallbackContext):
                 help_text = help_list
                 help_buttons = []
             text = (
-                "Here is the help for the *{}* ᴍᴏᴅᴜʟᴇ:\n".format(
+                "*{}*:\n\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + help_text
@@ -451,7 +451,7 @@ def get_help(update: Update, context: CallbackContext):
             elif isinstance(help_list, str):
                 help_text = help_list
             text = (
-                "Here is the available help for the *{}* ᴍᴏᴅᴜʟᴇ:\n".format(
+                "*{}*:\n\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + help_text
