c10ddd6aa96c37ea0c2edd099de5a52b28e5a951
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-08-30 03:18:49 +0530

    Update readable_time.py

--- Exon/modules/helper_funcs/readable_time.py
+++ Exon/modules/helper_funcs/readable_time.py
@@ -27,7 +27,7 @@ def get_readable_time(seconds: int) -> str:
     count = 0
     readable_time = ""
     time_list = []
-    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
+    time_suffix_list = ["S", "M", "H", "DAYS"]
 
     while count < 4:
         count += 1
