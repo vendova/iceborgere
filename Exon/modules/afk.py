245d3af1c43985addd58dbab16db66db938e909a
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-01 01:14:09 +0530

    Update afk.py

--- Exon/modules/afk.py
+++ Exon/modules/afk.py
@@ -29,6 +29,7 @@ SOFTWARE.
 
 
 import time
+import asyncio
 import random
 from Abg.helpers.human_read import get_readable_time
 from pyrogram.types import Message
@@ -57,7 +58,7 @@ async def active_afk(_, message: Message):
                     f"**{message.from_user.first_name}** is back online and was away for {seenago}",
                     disable_web_page_preview=True,
                 )
-                time.sleep(6)
+                asyncio.sleep(6)
                 await send.delete()
 
             if afktype == "text_reason":
@@ -65,7 +66,7 @@ async def active_afk(_, message: Message):
                     f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
                     disable_web_page_preview=True,
                 )
-                time.sleep(6)
+                asyncio.sleep(6)
                 await send.delete()
 
             if afktype == "animation":
@@ -74,14 +75,14 @@ async def active_afk(_, message: Message):
                         data,
                         caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}",
                     )
-                    time.sleep(6)
+                    asyncio.sleep(6)
                     await send.delete()
                 else:
                     send = await message.reply_animation(
                         data,
                         caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
                     )
-                    time.sleep(6)
+                    asyncio.sleep(6)
                     await send.delete()
 
             if afktype == "photo":
@@ -90,21 +91,21 @@ async def active_afk(_, message: Message):
                         photo=f"downloads/{user_id}.jpg",
                         caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}",
                     )
-                    time.sleep(6)
+                    asyncio.sleep(6)
                     await send.delete()
                 else:
                     send = await message.reply_photo(
                         photo=f"downloads/{user_id}.jpg",
                         caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
                     )
-                    time.sleep(6)
+                    asyncio.sleep(6)
                     await send.delete()
         except Exception:
             send = await message.reply_text(
                 f"**{message.from_user.first_name}** is back online.",
                 disable_web_page_preview=True,
             )
-            time.sleep(6)
+            asyncio.sleep(6)
             await send.delete()
 
     if len(message.command) == 1 and not message.reply_to_message:
@@ -249,7 +250,7 @@ async def active_afk(_, message: Message):
     await message.reply_sticker(random.choice(sticker_ids))
     gone = await message.reply_text(f"{message.from_user.first_name} is now afk!")
 # Add a delay of 6 seconds before deleting the message
-    time.sleep(6)
+    asyncio.sleep(6)
     await gone.delete()
 
 
