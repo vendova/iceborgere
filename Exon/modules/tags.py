3ef64fb21eca31182d80c9192c634ecc0e6a5344
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-01 04:40:43 +0530

    Update tags.py

--- Exon/modules/tags.py
+++ Exon/modules/tags.py
@@ -64,6 +64,8 @@ def get_info(id):
 @AsuX.on_message(filters.command(["tagalert", "afk"]))
 async def locks_dfunc(_, message):
     lol = await message.reply("__who has username 🔉❓...")
+    await asyncio.sleep(12)
+    await lol.delete()
     if len(message.command) != 2:
         if len(message.command) == 1 and message.command[0] in ["tagalert", "afk", "start"]:
             parameter = "on"
@@ -115,8 +117,6 @@ async def locks_dfunc(_, message):
         await lol.edit("Expected ON or OFF")
         await asyncio.sleep(2)
         await lol.delete()
-    await asyncio.sleep(12)
-    await lol.delete()
 
 @AsuX.on_message(filters.incoming)
 async def mentioned_alert(client, message):
@@ -356,90 +356,6 @@ async def mentioned_alert(client, message):
         message.continue_propagation()
     except:
         return message.continue_propagation()
-
-@abishnoi.on(events.NewMessage(pattern="^/tagall ?(.*)"))
-@abishnoi.on(events.NewMessage(pattern="^@all ?(.*)"))
-async def mentionall(event):
-    chat_id = event.chat_id
-    if event.is_private:
-        return await event.respond("__ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ ʙᴇ ᴜsᴇ ɪɴ ɢʀᴏᴜᴘs !__")
-
-    is_admin = False
-    try:
-        partici_ = await abishnoi(GetParticipantRequest(event.chat_id, event.sender_id))
-    except UserNotParticipantError:
-        is_admin = False
-    else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
-        ):
-            is_admin = True
-    if not is_admin:
-        return await event.respond("__ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴍᴇɴᴛɪᴏɴ ᴀʟʟ !__")
-
-    if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("__ɢɪᴠᴇ ᴍᴇ ᴏɴᴇ ᴀʀɢᴜᴍᴇɴᴛ ʙᴀʙʏ!__")
-    elif event.pattern_match.group(1):
-        mode = "text_on_cmd"
-        msg = event.pattern_match.group(1)
-    elif event.is_reply:
-        mode = "text_on_reply"
-        msg = await event.get_reply_message()
-        if msg == None:
-            return await event.respond(
-                "__I ᴄᴀɴ'ᴛ ᴍᴇɴᴛɪᴏɴ ᴍᴇᴍʙᴇʀs ғᴏʀ ᴏʟᴅᴇʀ ᴍᴇssᴀɢᴇs! (ᴍᴇssᴀɢᴇs ᴡʜɪᴄʜ ᴀʀᴇ sᴇɴᴛ ʙᴇғᴏʀᴇ I'ᴍ ᴀᴅᴅᴇᴅ ᴛᴏ ɢʀᴏᴜᴘ)__"
-            )
-    else:
-        return await event.respond(
-            "__ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ ᴍᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴏᴛʜᴇʀs!__"
-        )
-
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in abishnoi.iter_participants(chat_id):
-        if not chat_id in spam_chats:
-            break
-        usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
-        if usrnum == 5:
-            if mode == "text_on_cmd":
-                txt = f"{msg}\n{usrtxt}"
-                await abishnoi.send_message(chat_id, txt)
-            elif mode == "text_on_reply":
-                await msg.reply(usrtxt)
-            await asyncio.sleep(3)
-            usrnum = 0
-            usrtxt = ""
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
-
-
-@abishnoi.on(events.NewMessage(pattern="^/cancel$"))
-async def cancel_spam(event):
-    if not event.chat_id in spam_chats:
-        return await event.respond("__ᴛʜᴇʀᴇ ɪs ɴᴏ ᴘʀᴏᴄᴄᴇss ᴏɴ ɢᴏɪɴɢ...__")
-    is_admin = False
-    try:
-        partici_ = await abishnoi(GetParticipantRequest(event.chat_id, event.sender_id))
-    except UserNotParticipantError:
-        is_admin = False
-    else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
-        ):
-            is_admin = True
-    if not is_admin:
-        return await event.respond("__ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ!__")
-
-    else:
-        try:
-            spam_chats.remove(event.chat_id)
-        except:
-            pass
-        return await event.respond("__sᴛᴏᴘᴘᴇᴅ ᴍᴇɴᴛɪᴏɴ.__")
 
 
 __mod_name__ = "Tags"
