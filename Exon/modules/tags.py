681f597c2e9310ed7feb8f096f9bc4800a44aa27
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-04 03:22:09 +0530

    Update tags.py

--- Exon/modules/tags.py
+++ Exon/modules/tags.py
@@ -1,1 +1,365 @@
-#none
+"""
+MIT License
+
+Copyright (c) 2022 ABISHNOI69
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE
+# TG :- @Abishnoi1M
+#      :- Abishnoi_bots
+#     GITHUB :- Abishnoi69 ""
+
+
+import asyncio
+
+from telethon import events
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from Exon import telethn as abishnoi
+
+spam_chats = []
+
+
+import asyncio
+from datetime import timedelta
+
+import dateparser
+from pyrogram import filters
+from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
+
+from Exon import Abishnoi as AsuX
+from Exon.modules.no_sql import AsuXdb as db
+
+approved_users = db.approve
+tagdb = db.tagdb1
+alarms = db.alarm
+shedule = db.shedule
+nightmod = db.nightmode4
+
+
+def get_info(id):
+    return nightmod.find_one({"id": id})
+
+
+@AsuX.on_message(filters.command(["tagalert", "afk"]))
+async def locks_dfunc(_, message):
+    lol = await message.reply("__who has username 🔉❓...")
+    if len(message.command) != 2:
+        if len(message.command) == 1 and message.command[0] in ["tagalert", "afk", "start"]:
+            parameter = "on"
+        else:
+            await lol.edit("Expected ON or OFF")
+            await asyncio.sleep(2)
+            await lol.delete()
+            return
+    else:
+        parameter = message.text.strip().split(None, 1)[1].lower()
+
+    if parameter in ["on", ""]:
+        if not message.from_user:
+            return
+        if not message.from_user.username:
+            return
+        uname = str(message.from_user.username)
+        uname = uname.lower()
+        isittrue = tagdb.find_one({f"teg": uname})
+        if not isittrue:
+            tagdb.insert_one({f"teg": uname})
+            await lol.edit(
+                f"Tag alerts ENABLED.\nWhen someone tags you as @{uname} you will be notified."
+            )
+            await asyncio.sleep(3)
+            await lol.delete()
+            return
+        else:
+            await lol.edit("Tag alerts already ENABLED for you!")
+            await asyncio.sleep(3)
+            await lol.delete()
+            return
+    if parameter == "off":
+        if not message.from_user:
+            return
+        if not message.from_user.username:
+            return await lol.edit(
+                "only users with USERNAMES are eligible for getting tagalerts; and you can't remove because you aren't eligible at the first place!"
+            )
+        uname = message.from_user.username
+        uname = uname.lower()
+        isittrue = tagdb.find_one({f"teg": uname})
+        if isittrue:
+            tagdb.delete_one({f"teg": uname})
+            return await lol.edit("Tag alerts REMOVED!")
+        else:
+            return await lol.edit("Tag alerts already DISABLED for you!")
+    else:
+        await lol.edit("Expected ON or OFF")
+        await asyncio.sleep(2)
+        await lol.delete()
+
+@AsuX.on_message(filters.incoming)
+async def mentioned_alert(client, message):
+    try:
+        if not message:
+            message.continue_propagation()
+            return
+        if not message.from_user:
+            message.continue_propagation()
+            return
+        input_str = message.text
+        input_str = input_str.lower()
+        if "@" in input_str:
+            input_str = input_str.replace("@", "  |")
+            inuka = input_str.split("|")[1]
+            text = inuka.split()[0]
+        else:
+            chats = alarms.find({})
+            for c in chats:
+                # print(c)
+                chat = c["chat"]
+                user = c["user"]
+                time = c["time"]
+                zone = c["zone"]
+                reason = c["reason"]
+                present = dateparser.parse(
+                    f"now", settings={"TIMEZONE": f"{zone}", "DATE_ORDER": "YMD"}
+                )
+                ttime = dateparser.parse(f"{time}", settings={"TIMEZONE": f"{zone}"})
+                # print(ttime)
+                # print(present)
+                # print (zone)
+                # print(present>=ttime)
+                if present > ttime:
+                    try:
+                        alarms.delete_one(
+                            {
+                                "chat": chat,
+                                "user": user,
+                                "time": time,
+                                "zone": zone,
+                                "reason": reason,
+                            }
+                        )
+                        await client.send_message(
+                            chat,
+                            f"**🚨 Reminder 🚨**\n\n__This is a reminder set by__ {user}\n__Reason__: {reason} \n\n`Reminded at: {ttime}`",
+                        )
+
+                        message.continue_propagation()
+                    except:
+                        alarms.delete_one(
+                            {
+                                "chat": chat,
+                                "user": user,
+                                "time": time,
+                                "zone": zone,
+                                "reason": reason,
+                            }
+                        )
+                        return message.continue_propagation()
+                    break
+                    return message.continue_propagation()
+                continue
+            chats = shedule.find({})
+            for c in chats:
+                # print(c)
+                chat = c["chat"]
+                user = c["user"]
+                time = c["time"]
+                zone = c["zone"]
+                reason = c["reason"]
+                present = dateparser.parse(
+                    f"now", settings={"TIMEZONE": f"{zone}", "DATE_ORDER": "YMD"}
+                )
+                ttime = dateparser.parse(f"{time}", settings={"TIMEZONE": f"{zone}"})
+                # print(ttime)alarms
+                # print(present)
+                # print (zone)
+                # print(present>=ttime)
+                if present > ttime:
+                    try:
+                        shedule.delete_one(
+                            {
+                                "chat": chat,
+                                "user": user,
+                                "time": time,
+                                "zone": zone,
+                                "reason": reason,
+                            }
+                        )
+                        await client.send_message(chat, f"{reason}")
+                        message.continue_propagation()
+                    except:
+                        shedule.delete_one(
+                            {
+                                "chat": chat,
+                                "user": user,
+                                "time": time,
+                                "zone": zone,
+                                "reason": reason,
+                            }
+                        )
+                        return message.continue_propagation()
+                    break
+                    return message.continue_propagation()
+                continue
+            chats = nightmod.find({})
+
+            for c in chats:
+                # print(c)
+                id = c["id"]
+                valid = c["valid"]
+                zone = c["zone"]
+                c["ctime"]
+                otime = c["otime"]
+                present = dateparser.parse(
+                    "now", settings={"TIMEZONE": f"{zone}", "DATE_ORDER": "YMD"}
+                )
+                try:
+                    if present > otime and valid:
+                        newtime = otime + timedelta(days=1)
+                        to_check = get_info(id=id)
+                        if not to_check:
+                            return message.continue_propagation()
+                        if not newtime:
+                            return message.continue_propagation()
+                        # print(newtime)
+                        # print(to_check)
+                        nightmod.update_one(
+                            {
+                                "_id": to_check["_id"],
+                                "id": to_check["id"],
+                                "valid": to_check["valid"],
+                                "zone": to_check["zone"],
+                                "ctime": to_check["ctime"],
+                                "otime": to_check["otime"],
+                            },
+                            {"$set": {"otime": newtime}},
+                        )
+                        await client.set_chat_permissions(
+                            id,
+                            ChatPermissions(
+                                can_send_messages=True,
+                                can_send_media_messages=True,
+                                can_send_stickers=True,
+                                can_send_animations=True,
+                            ),
+                        )
+
+                        await client.send_message(
+                            id,
+                            "**🌗 Night mode ENDED: `chat will be opened` \n\n Everyone will be able to send messages.**",
+                        )
+                        message.continue_propagation()
+                        break
+                        return message.continue_propagation()
+                except:
+                    print("Chat open error in nightbot")
+                    return message.continue_propagation()
+                continue
+            chats = nightmod.find({})
+            for c in chats:
+                # print(c)
+                id = c["id"]
+                valid = c["valid"]
+                zone = c["zone"]
+                ctime = c["ctime"]
+                c["otime"]
+                c["otime"]
+                present = dateparser.parse(
+                    "now", settings={"TIMEZONE": f"{zone}", "DATE_ORDER": "YMD"}
+                )
+                try:
+                    if present > ctime and valid:
+                        newtime = ctime + timedelta(days=1)
+                        to_check = get_info(id=id)
+                        if not to_check:
+                            return message.continue_propagation()
+                        if not newtime:
+                            return message.continue_propagation()
+                        # print(newtime)
+                        # print(to_check)
+                        nightmod.update_one(
+                            {
+                                "_id": to_check["_id"],
+                                "id": to_check["id"],
+                                "valid": to_check["valid"],
+                                "zone": to_check["zone"],
+                                "ctime": to_check["ctime"],
+                                "otime": to_check["otime"],
+                            },
+                            {"$set": {"ctime": newtime}},
+                        )
+                        await client.set_chat_permissions(id, ChatPermissions())
+                        await client.send_message(
+                            id,
+                            "**🌗Night mode starting: `Chat close initiated`\n\nOnly admins should be able to send messages.**",
+                        )
+                        message.continue_propagation()
+                        break
+                        return message.continue_propagation()
+                except:
+                    print("Chat close err")
+                    return message.continue_propagation()
+                continue
+            return message.continue_propagation()
+        # print(text)
+        if tagdb.find_one({f"teg": text}):
+            pass
+        else:
+            return message.continue_propagation()
+        # print("Im inn")
+        try:
+            chat_name = message.chat.title
+            message.chat.id
+            tagged_msg_link = message.link
+        except:
+            return message.continue_propagation()
+        user_ = message.from_user.mention or f"@{message.from_user.username}"
+
+        final_tagged_msg = f"**❗{user_} ** [mentioned]({tagged_msg_link}) you in {chat_name}!"
+        button_s = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("View message", url=tagged_msg_link)]]
+        )
+        # print(final_tagged_msg)
+        try:
+            await client.send_message(
+                chat_id=f"{text}",
+                text=final_tagged_msg,
+                reply_markup=button_s,
+                disable_web_page_preview=True,
+            )
+
+        except:
+            return message.continue_propagation()
+        message.continue_propagation()
+    except:
+        return message.continue_propagation()
+
+
+__mod_name__ = "Tags"
+
+from Exon.modules.language import gs
+
+
+def get_help(chat):
+    return gs(chat, "tags_help")
