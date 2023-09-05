6aaf7378070b7250c0ac31fb7332253aca2f14ad
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-05 20:25:37 +0530

    Update purge.py

--- Exon/modules/purge.py
+++ Exon/modules/purge.py
@@ -1,153 +1,307 @@
-"""
-MIT License
+""" 
 
-Copyright (c) 2022 ABISHNOI69
+MIT License 
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+Copyright (c) 2022 ABISHNOI69 
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+  
 
-# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE
-# TG :- @Abishnoi1m
-#     UPDATE   :- Abishnoi_bots
-#     GITHUB :- ABISHNOI69 ""
+Permission is hereby granted, free of charge, to any person obtaining a copy 
 
+of this software and associated documentation files (the "Software"), to deal 
 
-from pyrogram.enums import ChatType
-from pyrogram.errors import MessageDeleteForbidden, RPCError
-from pyrogram.types import Message
+in the Software without restriction, including without limitation the rights 
 
-from Exon import LOGGER, Abishnoi
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
 
+copies of the Software, and to permit persons to whom the Software is 
 
-@Abishnoi.on_cmd("purge")
-@Abishnoi.adminsOnly(permissions="can_delete_messages", is_both=True)
-async def purge(c: Abishnoi, m: Message):
-    if m.chat.type != ChatType.SUPERGROUP:
-        await m.reply_text(text="cANNoT PURGE MEssAGEs IN A BAsIc GRoUP")
-        return
+furnished to do so, subject to the following conditions: 
 
-    if m.reply_to_message:
-        message_ids = list(range(m.reply_to_message.id, m.id))
+  
 
-        def divide_chunks(l: list, n: int = 100):
-            for i in range(0, len(l), n):
-                yield l[i : i + n]
+The above copyright notice and this permission notice shall be included in all 
 
-        # Dielete messages in chunks of 100 messages
-        m_list = list(divide_chunks(message_ids))
+copies or substantial portions of the Software. 
 
-        try:
-            for plist in m_list:
-                await c.delete_messages(
-                    chat_id=m.chat.id,
-                    message_ids=plist,
-                    revoke=True,
-                )
-            await m.delete()
-        except MessageDeleteForbidden:
-            await m.reply_text(
-                text="cANNoT DELETE ALL MEssAGEs. THE MEssAGEs MAY BE Too oLD, I MIGHT NoT HAvE DELETE RIGHTs, oR THIs MIGHT NoT BE A sUPERGRoUP."
-            )
-            return
-        except RPCError as ef:
-            LOGGER.info(f"ERROR on purge {ef}")
+  
 
-        count_del_msg = len(message_ids)
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
 
-        z = await m.reply_text(text=f"DELETED <i>{count_del_msg}</i> messages")
-        return
-    await m.reply_text("REPLY To A MEssAGE To sTART PURGE !")
-    return
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
 
-@Abishnoi.on_cmd("spurge")
-@Abishnoi.adminsOnly(permissions="can_delete_messages", is_both=True)
-async def spurge(c: Abishnoi, m: Message):
-    if m.chat.type != ChatType.SUPERGROUP:
-        await m.reply_text(text="cANNoT PURGE MEssAGEs IN A BAsIc GRoUP")
-        return
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
 
-    if m.reply_to_message:
-        message_ids = list(range(m.reply_to_message.id, m.id))
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
 
-        def divide_chunks(l: list, n: int = 100):
-            for i in range(0, len(l), n):
-                yield l[i : i + n]
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
 
-        # Dielete messages in chunks of 100 messages
-        m_list = list(divide_chunks(message_ids))
+SOFTWARE. 
 
-        try:
-            for plist in m_list:
-                await c.delete_messages(
-                    chat_id=m.chat.id,
-                    message_ids=plist,
-                    revoke=True,
-                )
-            await m.delete()
-        except MessageDeleteForbidden:
-            await m.reply_text(
-                text="cANNoT DELETE ALL MEssAGEs. THE MEssAGEs MAY BE Too oLD, I MIGHT NoT HAvE DELETE RIGHTs, oR THIs MIGHT NoT BE A sUPERGRoUP."
-            )
-            return
-        except RPCError as ef:
-            LOGGER.info(f"ERROR on purge {ef}")
-        return
-    await m.reply_text("REPLY To A MEssAGE To sTART sPURGE !")
-    return
+""" 
 
+  
 
-@Abishnoi.on_cmd("del")
-@Abishnoi.adminsOnly(permissions="can_delete_messages", is_both=True)
-async def del_msg(c: Abishnoi, m: Message):
-    if m.reply_to_message:
-        await m.delete()
-        await c.delete_messages(
-            chat_id=m.chat.id,
-            message_ids=m.reply_to_message.id,
-        )
-    else:
-        await m.reply_text(text="WHAT Do YoU WANNA DELETE?")
-    return
+# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE 
 
+# TG :- @Abishnoi1m 
 
-__PLUGIN__ = "PURGE"
+#     UPDATE   :- Abishnoi_bots 
 
-__alt_name__ = ["purge", "del", "spurge"]
+#     GITHUB :- ABISHNOI69 "" 
 
-__HELP__ = """
-• /purge: DELETEs MEssAGEs UPTo REPLIED MEssAGE.
-• /spurge: DELETEs MEssAGEs UPTo REPLIED MEssAGE WITHoUT A sUccEss MEssAGE.
-• /del: DELETEs A sINGLE MEssAGE, UsED As A REPLY To MEssAGE.
-"""
+  
 
-__mod_name__ = "Purge"
+  
 
-# foR HELP MENU
+from pyrogram.enums import ChatType 
 
+from pyrogram.errors import MessageDeleteForbidden, RPCError 
 
-# """
-from Exon.modules.language import gs
+from pyrogram.types import Message 
 
+  
 
-def get_help(chat):
-    return gs(chat, "purge_help")
+from Exon import LOGGER, Abishnoi 
 
+  
 
-# """
+  
+
+@Abishnoi.on_cmd("purge") 
+
+@Abishnoi.adminsOnly(permissions="can_delete_messages", is_both=True) 
+
+async def purge(c: Abishnoi, m: Message): 
+
+    if m.chat.type != ChatType.SUPERGROUP: 
+
+        await m.reply_text(text="cannot purge messages in a basic group") 
+
+        return 
+
+  
+
+    if m.reply_to_message: 
+
+        message_ids = list(range(m.reply_to_message.id, m.id)) 
+
+  
+
+        def divide_chunks(l: list, n: int = 100): 
+
+            for i in range(0, len(l), n): 
+
+                yield l[i : i + n] 
+
+  
+
+        # Dielete messages in chunks of 100 messages 
+
+        m_list = list(divide_chunks(message_ids)) 
+
+  
+
+        try: 
+
+            for plist in m_list: 
+
+                await c.delete_messages( 
+
+                    chat_id=m.chat.id, 
+
+                    message_ids=plist, 
+
+                    revoke=True, 
+
+                ) 
+
+            await m.delete() 
+
+        except MessageDeleteForbidden: 
+
+            await m.reply_text( 
+
+                text="cannot delete all messages. the messages may be too old, i might not have delete rights, or this might not be a supergroup." 
+
+            ) 
+
+            return 
+
+        except RPCError as ef: 
+
+            LOGGER.info(f"ERROR on purge {ef}") 
+
+  
+
+        count_del_msg = len(message_ids) 
+
+  
+
+        z = await m.reply_text(text=f"DELETED <i>{count_del_msg}</i> messages") 
+
+        return 
+
+    await m.reply_text("Reply to a message to start purge !") 
+
+    return 
+
+  
+
+  
+
+@Abishnoi.on_cmd("spurge") 
+
+@Abishnoi.adminsOnly(permissions="can_delete_messages", is_both=True) 
+
+async def spurge(c: Abishnoi, m: Message): 
+
+    if m.chat.type != ChatType.SUPERGROUP: 
+
+        await m.reply_text(text="cannot purge messages in a basic group") 
+
+        return 
+
+  
+
+    if m.reply_to_message: 
+
+        message_ids = list(range(m.reply_to_message.id, m.id)) 
+
+  
+
+        def divide_chunks(l: list, n: int = 100): 
+
+            for i in range(0, len(l), n): 
+
+                yield l[i : i + n] 
+
+  
+
+        # Dielete messages in chunks of 100 messages 
+
+        m_list = list(divide_chunks(message_ids)) 
+
+  
+
+        try: 
+
+            for plist in m_list: 
+
+                await c.delete_messages( 
+
+                    chat_id=m.chat.id, 
+
+                    message_ids=plist, 
+
+                    revoke=True, 
+
+                ) 
+
+            await m.delete() 
+
+        except MessageDeleteForbidden: 
+
+            await m.reply_text( 
+
+                text="cannot delete all messages. the messages may be too old, i might not have delete rights, or this might not be a supergroup." 
+
+            ) 
+
+            return 
+
+        except RPCError as ef: 
+
+            LOGGER.info(f"ERROR on purge {ef}") 
+
+        return 
+
+    await m.reply_text("Reply to a message to start spurge !") 
+
+    return 
+
+  
+
+  
+
+@Abishnoi.on_cmd("del") 
+
+@Abishnoi.adminsOnly(permissions="can_delete_messages", is_both=True) 
+
+async def del_msg(c: Abishnoi, m: Message): 
+
+    if m.reply_to_message: 
+
+        await m.delete() 
+
+        await c.delete_messages( 
+
+            chat_id=m.chat.id, 
+
+            message_ids=m.reply_to_message.id, 
+
+        ) 
+
+    else: 
+
+        await m.reply_text(text="What do you wanna delete?") 
+
+    return 
+
+  
+
+  
+
+__PLUGIN__ = "PURGE" 
+
+  
+
+__alt_name__ = ["purge", "del", "spurge"] 
+
+  
+
+__HELP__ = """ 
+
+• /purge: Deletes messages upto replied message. 
+
+• /spurge: Deletes messages upto replied message without a success message. 
+
+• /del: Deletes a single message, used as a reply to message. 
+
+""" 
+
+  
+
+__mod_name__ = "Purge" 
+
+  
+
+# foR HELP MENU 
+
+  
+
+  
+
+# """ 
+
+from Exon.modules.language import gs 
+
+  
+
+  
+
+def get_help(chat): 
+
+    return gs(chat, "purge_help") 
+
+  
+
+  
+
+# """ 
+
+ 
