58e68efce33636df8c2c7fedfd18cdd804a450a7
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-07 01:31:40 +0530

    Update blacklist.py

--- Exon/modules/blacklist.py
+++ Exon/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.blacklist_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.warns import warn
+import Exon.modules.sql.blacklist_sql as sql
+from Exon import LOGGER, dispatcher
+from Exon.modules.connection import connected
+from Exon.modules.disable import DisableAbleCommandHandler
+from Exon.modules.helper_funcs.alternate import send_message, typing_action
+from Exon.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Exon.modules.helper_funcs.extraction import extract_text
+from Exon.modules.helper_funcs.misc import split_message
+from Exon.modules.helper_funcs.string_handling import extract_time
+from Exon.modules.log_channel import loggable
+from Exon.modules.sql.approve_sql import is_approved
+from Exon.modules.warns import warn
 
 BLACKLIST_GROUP = 11
 
