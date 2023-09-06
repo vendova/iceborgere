25b8a18812de8ce61f3bdf6689c4c8a164983037
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-07 02:14:12 +0530

    Update mongo.py

--- Exon/utils/mongo.py
+++ Exon/utils/mongo.py
@@ -1,9 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from FallenRobot import MONGO_DB_URI
+from Exon import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.FallenRobot
+db = mongo.Exon
 
 coupledb = db.couple
 
