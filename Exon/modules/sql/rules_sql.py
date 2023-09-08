d26c2a31bb9908e4ee7e9994a4fe253b146f5e4c
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-08 13:24:54 +0530

    Update rules_sql.py

--- Exon/modules/sql/rules_sql.py
+++ Exon/modules/sql/rules_sql.py
@@ -1,31 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2022 ABISHNOI69
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE
-# TG :- @Abishnoi1m
-#     UPDATE   :- Abishnoi_bots
-#     GITHUB :- ABISHNOI69 ""
 import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func
@@ -63,7 +35,10 @@ def set_rules(chat_id, rules_text):
 
 def get_rules(chat_id):
     rules = SESSION.query(Rules).get(str(chat_id))
-    ret = rules.rules if rules else ""
+    ret = ""
+    if rules:
+        ret = rules.rules
+
     SESSION.close()
     return ret
 
