25c6ba254869d5a1458c5e455e06a26ec9c35923
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-10 02:59:11 +0530

    Update reporting.py

--- Exon/modules/reporting.py
+++ Exon/modules/reporting.py
@@ -17,10 +17,10 @@
 
 import html
 import logging
-from typing import Optional
+from typing import List, Optional
 from telegram import Message, Chat, Update, User, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.error import BadRequest, Unauthorized
-from telegram.ext import CommandHandler, CallbackQueryHandler, Filters, MessageHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler, Filters, MessageHandler, RegexHandler, run_async
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.utils.helpers import mention_html
 from config import Config
@@ -32,11 +32,9 @@ from Exon import dispatcher
 from Exon import EVENT_LOGS, LOGGER, dispatcher
 REPORT_GROUP = 12
 
-where_is = str(EVENT_LOGS)
-
+@run_async
 @user_admin
-def report_setting(update: Update, context: CallbackContext):
-    args = context.args
+def report_setting(bot: Bot, update: Update, args: List[str]):
     chat = update.effective_chat  # type: Optional[Chat]
     msg = update.effective_message  # type: Optional[Message]
 
@@ -69,9 +67,10 @@ def report_setting(update: Update, context: CallbackContext):
                            parse_mode=ParseMode.MARKDOWN)
 
 
+@run_async
 @user_not_admin
 @loggable
-def report(update: Update, context: CallbackContext) -> str:
+def report(bot: Bot, update: Update) -> str:
     message = update.effective_message  # type: Optional[Message]
     chat = update.effective_chat  # type: Optional[Chat]
     user = update.effective_user  # type: Optional[User]
@@ -126,7 +125,6 @@ def report(update: Update, context: CallbackContext) -> str:
                                     message.reply_to_message.message_id))
                         ]]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            msg = f"debugging EVENT_LOGS from {where_is} \ndebugging LOG_GROUP_ID from {get_chat_log_channel}"
 
         else:
             msg = "{} is calling for admins in \"{}\"!".format(
@@ -140,29 +138,56 @@ def report(update: Update, context: CallbackContext) -> str:
 
             if sql.user_should_report(admin.user.id):
                 try:
-                    context.bot.send_message(-1001925908750,
-                                             msg + link,
-                                             parse_mode=ParseMode.HTML,
-                                             disable_web_page_preview=True)
+                    if not chat.type == Chat.SUPERGROUP:
+                        bot.send_message(admin.user.id,
+                                         msg + link,
+                                         parse_mode=ParseMode.HTML)
 
-                    if should_forward:
-                        message.reply_to_message.forward(admin.user.id)
+                        if should_forward:
+                            message.reply_to_message.forward(admin.user.id)
 
-                        if len(
-                                message.text.split()
-                        ) > 1:  # If user is giving a reason, send his message too
-                            message.forward(admin.user.id)
+                            if len(
+                                    message.text.split()
+                            ) > 1:  # If user is giving a reason, send his message too
+                                message.forward(admin.user.id)
+
+                    if not chat.username:
+                        bot.send_message(admin.user.id,
+                                         msg + link,
+                                         parse_mode=ParseMode.HTML)
+
+                        if should_forward:
+                            message.reply_to_message.forward(admin.user.id)
+
+                            if len(
+                                    message.text.split()
+                            ) > 1:  # If user is giving a reason, send his message too
+                                message.forward(admin.user.id)
+
+                    if chat.username and chat.type == Chat.SUPERGROUP:
+                        bot.send_message(admin.user.id,
+                                         msg + link,
+                                         parse_mode=ParseMode.HTML,
+                                         reply_markup=reply_markup)
+
+                        if should_forward:
+                            message.reply_to_message.forward(admin.user.id)
+
+                            if len(
+                                    message.text.split()
+                            ) > 1:  # If user is giving a reason, send his message too
+                                message.forward(admin.user.id)
 
                 except Unauthorized:
                     pass
                 except BadRequest as excp:  # TODO: cleanup exceptions
-                    logging.error(f"Exception while reporting user : {excp}")
+                    LOGGER.exception(
+                        f"Exception while reporting user : {excp}")
 
         message.reply_to_message.reply_text((
             chat.id,
             "reports_success").format(mention_html(user.id, user.first_name)),
-                                            parse_mode=ParseMode.HTML,
-                                            disable_web_page_preview=True)
+                                            parse_mode=ParseMode.HTML)
         return msg
 
     return ""
@@ -172,58 +197,51 @@ def __migrate__(old_chat_id, new_chat_id):
     sql.migrate_chat(old_chat_id, new_chat_id)
 
 
-def buttons(update: Update, context: CallbackContext):
+def buttons(bot: Bot, update):
     query = update.callback_query
     splitter = query.data.replace("report_", "").split("=")
     if splitter[1] == "kick":
         try:
-            context.bot.kickChatMember(splitter[0], splitter[2])
-            context.bot.unbanChatMember(splitter[0], splitter[2])
+            bot.kickChatMember(splitter[0], splitter[2])
+            bot.unbanChatMember(splitter[0], splitter[2])
             query.answer("✅ Succesfully kicked")
             return ""
         except Exception as err:
             query.answer("❎ Failed to kick")
-            context.bot.sendMessage(text="Error: {}".format(err),
-                                    chat_id=query.message.chat_id,
-                                    parse_mode=ParseMode.HTML)
+            bot.sendMessage(text="Error: {}".format(err),
+                            chat_id=query.message.chat_id,
+                            parse_mode=ParseMode.HTML)
     elif splitter[1] == "banned":
         try:
-            context.bot.kickChatMember(splitter[0], splitter[2])
+            bot.kickChatMember(splitter[0], splitter[2])
             query.answer("✅  Succesfully Banned")
             return ""
         except Exception as err:
-            context.bot.sendMessage(text="Error: {}".format(err),
-                                    chat_id=query.message.chat_id,
-                                    parse_mode=ParseMode.HTML)
+            bot.sendMessage(text="Error: {}".format(err),
+                            chat_id=query.message.chat_id,
+                            parse_mode=ParseMode.HTML)
             query.answer("❎ Failed to ban")
     elif splitter[1] == "delete":
         try:
-            context.bot.deleteMessage(splitter[0], splitter[3])
+            bot.deleteMessage(splitter[0], splitter[3])
             query.answer("✅ Message Deleted")
             return ""
         except Exception as err:
-            context.bot.sendMessage(text="Error: {}".format(err),
-                                    chat_id=query.message.chat_id,
-                                    parse_mode=ParseMode.HTML)
+            bot.sendMessage(text="Error: {}".format(err),
+                            chat_id=query.message.chat_id,
+                            parse_mode=ParseMode.HTML)
             query.answer("❎ Failed to delete message!")
 
 
 __help__ = True
 
-REPORT_HANDLER = CommandHandler(["report", "admin"],
-                                report,
-                                run_async=True,
-                                filters=Filters.chat_type.groups)
-SETTING_HANDLER = CommandHandler("reports",
-                                 report_setting,
-                                 pass_args=True,
-                                 run_async=True)
-ADMIN_REPORT_HANDLER = MessageHandler(Filters.regex("(?i)@admin(s)?"),
-                                      report,
-                                      run_async=True)
+REPORT_HANDLER = CommandHandler("report", report, filters=Filters.group)
+SETTING_HANDLER = CommandHandler("reports", report_setting, pass_args=True)
+ADMIN_REPORT_HANDLER = RegexHandler("(?i)@admin(s)?", report)
 
 report_button_user_handler = CallbackQueryHandler(buttons, pattern=r"report_")
 dispatcher.add_handler(report_button_user_handler)
+
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(SETTING_HANDLER)
