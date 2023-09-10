4f323bd38b95716b81b7d4db82719b4e38939fd2
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-09-10 12:23:30 +0530

    Update mongo.py

--- Exon/utils/mongo.py
+++ Exon/utils/mongo.py
@@ -5,6 +5,7 @@ mongo = MongoCli("mongodb+srv://mogenius:mogenius@quickers.rlx3b3f.mongodb.net/?
 db = mongo.Exon
 
 coupledb = db.couple
+nsfwdb = db.nsfw
 
 
 async def _get_lovers(chat_id: int):
@@ -32,3 +33,23 @@ async def save_couple(chat_id: int, date: str, couple: dict):
         {"$set": {"couple": lovers}},
         upsert=True,
     )
+
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = await nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def nsfw_on(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return await nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return await nsfwdb.insert_one({"chat_id": chat_id})
